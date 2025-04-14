659e72317b0c5522ec7cfb5991834a476c84dea8
iara.vsp <iaravsp@al.insper.edu.br>
2025-04-14 10:11:54 -0300

    exe1 ok

--- exe1/main.c
+++ exe1/main.c
@@ -30,6 +30,10 @@ void i2c_task(void *p) {
 
     // TODO
     // Configure o acc para operar em 4G
+    uint8_t buf_4g[2];
+    buf_4g[0] = MPUREG_ACCEL_CONFIG;
+    buf_4g[1] = 1 << 3;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_4g, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
